505f6a18cfaa54ccda2608a8fbb56e5982351b4e
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-06-18 17:48:01 +0000

    Update starter kit for v1.77.0-rc.3

---
+++ StarterKit/components/InspectorBar+Postcard.swift
@@ -0,0 +1,35 @@
+import IMGLYEditor
+
+// MARK: - Inspector Bar
+
+extension PostcardEditorConfiguration {
+  /// The default inspector bar configuration.
+  static var defaultInspectorBar: InspectorBar.Configuration {
+    InspectorBar.Configuration { builder in
+      builder.items { _ in
+        InspectorBar.Buttons.replace()
+
+        InspectorBar.Buttons.editText()
+        InspectorBar.Buttons.formatText()
+        InspectorBar.Buttons.textOnPath()
+        InspectorBar.Buttons.fillStroke()
+        InspectorBar.Buttons.textBackground()
+        InspectorBar.Buttons.textStylePresets()
+        InspectorBar.Buttons.crop()
+
+        InspectorBar.Buttons.adjustments()
+        InspectorBar.Buttons.filter()
+        InspectorBar.Buttons.effect()
+        InspectorBar.Buttons.blur()
+        InspectorBar.Buttons.shape()
+
+        InspectorBar.Buttons.selectGroup()
+        InspectorBar.Buttons.enterGroup()
+
+        InspectorBar.Buttons.layer()
+        InspectorBar.Buttons.duplicate()
+        InspectorBar.Buttons.delete()
+      }
+    }
+  }
+}
